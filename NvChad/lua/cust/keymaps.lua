5c4e7ccb742a05dc4929addc9c02f5c196ed002d
Rustam Uzairov <mr.uzairov@gmail.com>
2023-04-16 22:12:43 +0000

    custom

--- NvChad/lua/cust/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- {{{ Global definitions
-
-Customize = require("config.customize")
-Functions = require("config.functions")
-Is_Enabled = Functions.is_enabled
--- ------------------------------------------------------------------------- }}}
-
-
-
