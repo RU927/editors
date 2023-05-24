084e12de941766f9b661c67aa66bd2c8b921b174
Rustam Uzairov <mr.uzairov@gmail.com>
2023-05-24 16:41:19 +0000

    after rename repo

--- v.sh
+++ v.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 rm -rf ~/.config/nvim-NvChad/lua/custom
 
-ln -vsf ~/REPOS/editors/nvim-NvChad/lua/custom ~/.config/nvim-NvChad/lua
-ln -vsf ~/REPOS/editors/tex ~/REPOS/editors/nvim-NvChad/lua/custom/
+ln -vsf ~/REPOS/re_writer/nvim-NvChad/lua/custom ~/.config/nvim-NvChad/lua
+ln -vsf ~/REPOS/re_writer/tex ~/REPOS/re_writer/nvim-NvChad/lua/custom/
 
-ln -svf ~/REPOS/editors/lvim ~/.config
+ln -svf ~/REPOS/re_writer/lvim ~/.config
 
 rm -rf ~/.config/nvim ~/.local/state/nvim ~/.local/share/nvim
 
