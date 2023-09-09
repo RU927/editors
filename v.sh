12beb0e89dbc23adfde7a6cccd604519211d42b3
Rustam Uzairov <mr.uzairov@gmail.com>
2023-09-10 01:41:30 +0300

    ..

--- v.sh
+++ v.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 rm -rf ~/.config/nvim-NvChad/lua/custom
 
-ln -vsf ~/REPOS/re_writer/nvim-NvChad/lua/custom ~/.config/nvim-NvChad/lua
-ln -vsf ~/REPOS/re_writer/tex ~/REPOS/re_writer/nvim-NvChad/lua/custom/
+ln -vsf ~/REPOS/re_viser/nvim-NvChad/lua/custom ~/.config/nvim-NvChad/lua
+ln -vsf ~/REPOS/re_viser/tex ~/REPOS/re_viser/nvim-NvChad/lua/custom/
 
-ln -svf ~/REPOS/re_writer/lvim ~/.config/
+ln -svf ~/REPOS/re_viser/lvim ~/.config/
 
 rm -rf ~/.config/nvim ~/.local/state/nvim ~/.local/share/nvim
 
